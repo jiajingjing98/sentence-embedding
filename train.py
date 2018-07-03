e2c2d476573eb7063055c623ce018d53dbf51abd
Jingjing Jia <jiajingjing@berkeley.edu>
2018-07-03 17:14:48 +0900

    d

--- train.py
+++ train.py
@@ -10,12 +10,15 @@ from torch import optim
 f = Encoder()
 sentences = ['Memories of childhood are unforgettable.', 'I was four years old when my grandfather died.',
              'I clearly remember how everybody in the house was weeping.']
+
+f.zero_grad()
+
 f.set_w2v_path(W2V_PATH)
 f.build_vocab(sentences, True)
 embeddings = f.encode(sentences, bsize=400, tokenize=False, verbose=True)
 
 print(embeddings)
-scores = np.matmul(embeddings,embeddings)
+scores = np.matmul(embeddings,np.transpose(embeddings))
 print(scores)
 scores_sum = np.sum(scores, axis=1, keepdims=True)
 
@@ -24,24 +27,19 @@ np.fill_diagonal(scores, 0)
 
 print(scores)
 
-targets = np.zeros((400,400))
+targets = np.zeros((3,3))
 context_size = 1
 
 ctxt_sent_pos = list(range(-context_size, context_size+1))
 ctxt_sent_pos.remove(0)
 for ctxt in ctxt_sent_pos:
-    targets += np.eye(400, k=ctxt)
+    targets += np.eye(3, k=ctxt)
 targets_sum = np.sum(targets,axis=1, keepdims=True)
 targets = targets / targets_sum
 print(targets)
 
-
-
-
-weight = torch.FloatTensor(2).fill_(1)
-loss_fn = nn.CrossEntropyLoss(weight=weight)
-loss_fn.size_average = False
-
-optim_fn = optim.Adam
-optimizer = optim_fn(f.parameters(), lr=0.0005)
-
+loss_fn = nn.CrossEntropyLoss()
+optimizer = optim.Adam(f.parameters(), lr=0.0005)
+loss = loss_fn(scores, targets)
+loss.backward()
+optimizer.step()
